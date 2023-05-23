f7f12c2f1e924451ea8c3bd04ea12e7432cbeed6
keanuharrell <keanu.harrell@viacesi.fr>
2023-05-23 11:45:59 +0200

    Changement du base

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwind from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
-import tailwindConfig from './tailwind.config.mjs';
+import tailwind from 'tailwindcss';
+import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import tailwindConfig from './tailwind.config.mjs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // If you are deploying to https://<USERNAME>.github.io/, set base to '/'.
   // If you are deploying to https://<USERNAME>.github.io/<REPO>/, for example your repository is at https://github.com/<USERNAME>/<REPO>, then set base to '/<REPO>/'.
-  base: '/gitprofile/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
